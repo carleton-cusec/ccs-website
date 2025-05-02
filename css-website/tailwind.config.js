6e0a6bb9acc99f8370a00a65e07bae6f1064457c
Anjali Patel <theanjali27@gmail.com>
2025-05-02 13:14:04 -0400

    feat(layout): set up header, footer, and layout component; added empty index.astro

--- css-website/tailwind.config.js
+++ css-website/tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{astro,html,js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        carletonRed: '#E91C24',
+        darkGray: '#121212',
+        mediumGray: '#1E1E1E',
+        lightGray: '#2A2A2A',
+        white: '#FFFFFF',
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'space': ['Space Grotesk', 'sans-serif'],
+      },
+    }
+  },
+  plugins: [],
+}
